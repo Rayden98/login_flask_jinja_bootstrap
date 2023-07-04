7592434035a8778ac3a4dbeda0356448119e13bf
Rayden98 <96914779+Rayden98@users.noreply.github.com>
2023-07-03 19:47:01 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+app.secret_key = "any-string"
+
+bootstrap = Bootstrap5(app)
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, max=30, message=None)])
+    submit = SubmitField(label='Log In')
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    email = "admin@email.com"
+    password = "12345678"
+
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if email == login_form.email.data and password == login_form.password.data:
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+
+    return render_template('login.html', form=login_form)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
